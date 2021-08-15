52757599361ca662dbaf68f43aaa4df67fc86d97
ANJALI MISHRA <64708739+anjali2804@users.noreply.github.com>
2021-08-15 11:55:12 +0530

    second commit

--- index.js
+++ index.js
@@ -36,23 +36,6 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
         agent.add(bot_response);
     });
     }
-  function employeeHandler(agent){
-    const first = agent.parameters.first_name;
-     const last = agent.parameters.last_name;
-     //agent.add(first);
-      return axios.get(`https://api10.successfactors.com/odata/v2/EmpEmployment?$format=json&custompagesize=1000&paging=cursor&$select=personIdExternal,userId,userNav/empId,userNav/defaultFullName,userNav/businessPhone,jobInfoNav/managerId,jobInfoNav/managerUserNav/defaultFullName,jobInfoNav/jobTitle,jobInfoNav/departmentNav/description&$expand=userNav,jobInfoNav/jobCodeNav,jobInfoNav/managerUserNav,jobInfoNav/departmentNav&$filter=jobInfoNav/emplStatus eq '1821' and (userNav/firstName like 'Rishabh%' and userNav/lastName like 'Goyal%')')`,{
-  auth: {
-    username: '80174@C0017935023T1',
-    password: 'Flipkart@123'
-  }
-}).then((result) => {
-        
-        const fullName=result.data.d.results[0].jobInfoNav.results.jobTitle;
-        console.log("job titleeeeeee",fullName);
-         var response=`job title of ${first} is ${fullName}`;
-  		agent.add(response);
-    });
-   }
   let intentMap = new Map();
   intentMap.set('Default Welcome Intent', welcome);
   intentMap.set('Default Fallback Intent', fallback);
